381c37a683b7655a82a736f44ae6fad0e79d5240
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-02-23 23:27:19 +0200

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# create a file
+# create school file
 
 file { 'school':
-  ensure  => 'present',
-  path    => '/tmp/school',
+  path     => '/tmp/school',
+  content  => 'I love Puppet',
   mode     => '0744',
   owner    => 'www-data',
   group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure   => 'present',
 }
