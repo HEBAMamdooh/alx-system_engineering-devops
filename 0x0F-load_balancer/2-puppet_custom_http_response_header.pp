3336a19f64c6fe46cf471fd67b35760d993abad9
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-03-04 23:26:55 +0200

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,42 @@
 # 2-puppet_custom_http_response_header.pp
+# This Puppet script configures Nginx on a new Ubuntu machine to include a custom HTTP header.
 
-# Creat custom HTTP header response
 package { 'nginx':
   ensure => installed,
 }
 
-# Define the custo HTTP header
-$http_header_value = $hostname
+file { '/etc/nginx/sites-available/default':
+  ensure => present,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-file { '/etc/nginx/conf.d/custom_header.conf':
-  ensure  => present,
-  content => 'add_header X-Served-By $http_header_value;',
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    add_header X-Served-By \$hostname;
+
+    location ~ \.php\$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}" 
 }
 
 service { 'nginx':
   ensure => running,
+  enable => true,
+  require => File['/etc/nginx/sites-available/default'],
 }
