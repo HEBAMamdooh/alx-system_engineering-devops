51e016ab31ae247290c6a492b16e905488027a78
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-03-04 21:30:19 +0200

    other tasks

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+2-puppet_custom_http_response_header.pp
+
+# Creat custom HTTP header response
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
