09eaf42e0de3ce7b8ea5a3f3ce9c598004c00125
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-03-04 23:21:37 +0200

    pupet file update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,18 @@
-2-puppet_custom_http_response_header.pp
+# 2-puppet_custom_http_response_header.pp
 
 # Creat custom HTTP header response
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define the custo HTTP header
+$http_header_value = $hostname
+
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => present,
+  content => 'add_header X-Served-By $http_header_value;',
+}
+
+service { 'nginx':
+  ensure => running,
 }
