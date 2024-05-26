ec23b1d697a3118f24712ed197d8a1eb7aab66b0
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-05-26 03:12:11 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,42 +1,46 @@
 # 2-puppet_custom_http_response_header.pp
 # This Puppet script configures Nginx on a new Ubuntu machine to include a custom HTTP header.
 
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    add_header X-Served-By \$hostname;
-
-    location ~ \.php\$ {
-        include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        include fastcgi_params;
-    }
-
-    location ~ /\.ht {
-        deny all;
-    }
-}" 
+# Configure Nginx for port 80 and a basic HTML page
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
 }
 
+# Allow HTTP traffic through the firewall
+exec { 'allow_nginx_http':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"',
+  path    => '/usr/sbin',
+  require => Package['nginx'],
+}
+
+# Create a custom configuration file for Nginx
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => file,
+  content => "add_header X-Served-By \$hostname;\n",
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  require => Package['nginx'],
+}
+
+# Ensure the custom header configuration is included in the default Nginx site configuration
+file_line { 'include_custom_header':
+  path  => '/etc/nginx/sites-available/default',
+  line  => 'include /etc/nginx/conf.d/custom_header.conf;',
+  match => '^include /etc/nginx/conf.d/custom_header.conf;$',
+  after => 'server {',
+  require => File['/etc/nginx/conf.d/custom_header.conf'],
+}
+
+# Ensure the Nginx service is running and restarted to apply changes
 service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/sites-available/default'],
+  ensure     => running,
+  enable     => true,
+  subscribe  => [File['/etc/nginx/conf.d/custom_header.conf'], File_line['include_custom_header'], File['/var/www/html/index.html']],
 }
