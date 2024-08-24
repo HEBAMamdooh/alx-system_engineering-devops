7a070fc694cc356f3065f04e8204cfa227c15e15
Heba Mamdooh <52792349+HEBAMamdooh@users.noreply.github.com>
2024-08-25 00:58:51 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,15 @@
 #Enable the user holberton to login and open files without errors
 
 # Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+file_line { 'increase-hard-file-limit-for-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton hard nofile 50000',
+  match => '^holberton hard nofile',
 }
 
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Increase soft file limit for holberton user
+file_line { 'increase-soft-file-limit-for-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton soft nofile 50000',
+  match => '^holberton soft nofile',
 }
